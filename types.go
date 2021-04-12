f19e053b50dad123cec3d6b7a33e11c59a6ab03c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-12 16:55:45 +0530

    types (using interface{})

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var x interface{}
@@ -23,4 +26,59 @@ func main() {
 		fmt.Println("Unknown type of %v = %T", v, v)
 
 	}
+
+	/*
+		var jsonBlob = []byte(`[
+				{"Name": "Platypus", "Order": "Monotremata", "Forest" : { "Country" : "India"}},
+				{"Name": "Quoll",    "Order": "Dasyuromorphia", "Forest" : { "Country" : "Brazil"} }
+			]`)
+		var animals []map[string]interface{}
+		err := json.Unmarshal(jsonBlob, &animals)
+		if err == nil {
+			for _, animal := range animals {
+				fmt.Println(animal["Name"], animal["Order"], animal["Forest"])
+			}
+		} else {
+			fmt.Println("error = ", err)
+		}
+	*/
+
+	fmt.Println("Sum of 10,20,30,40 = ", sum(10, 20, 30, 40))
+}
+
+/*
+Use cases:
+sum(10,20) => 30
+sum(10,20,30,40,50) => 150
+sum(10) => 10
+sum() => 0
+sum(10,20,30,"40") => 100
+sum(10, "20", 30 , "abc") => 60
+sum(10,20, []int{30,40,50}) => 150
+sum(10,20, []interface{30,40,"50"}) => 150
+*/
+
+func sum(nos ...interface{}) int {
+	result := 0
+	for _, no := range nos {
+		switch no.(type) {
+		case int:
+			result += no.(int)
+		case string:
+			if val, err := strconv.Atoi(no.(string)); err == nil {
+				result += val
+			}
+		case []int:
+			noList := no.([]int)
+			intfList := make([]interface{}, len(noList))
+			for idx, x := range noList {
+				intfList[idx] = x
+			}
+			result += sum(intfList...)
+		case []interface{}:
+			result += sum(no.([]interface{})...)
+		}
+
+	}
+	return result
 }
