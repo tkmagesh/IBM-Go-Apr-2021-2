d006128b850aa322f1be5dd6bc3ea606cc43fe5b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-12 18:10:37 +0530

    synchronized execution of goroutines using channels

--- concurrency/demo-03.go
+++ concurrency/demo-03.go
@@ -1,16 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func print(message string /*  */) {
-	fmt.Println(message)
+func print(message string, delay time.Duration, in chan string, out chan string) {
+	for {
+		<-in
+		time.Sleep(delay * time.Second)
+		fmt.Println(message)
+		out <- "done"
+	}
 }
 
 func main() {
 	/* Hello & World shoule be printed alternatively */
-	print("Hello") /* print with the delay of 1 sec */
-	print("World") /* print with the delay of 2 sec */
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go print("Hello", 1, ch1, ch2) /* print with the delay of 1 sec */
+	go print("World", 2, ch2, ch1) /* print with the delay of 2 sec */
+	ch1 <- "start"
 	var input string
 	fmt.Println("Enter any key to exit")
-	fmt.Scanf(&input)
+	fmt.Scanln(&input)
 }
