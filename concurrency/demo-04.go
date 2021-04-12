362c453d120907bff7e08e0f23b8a9fe34bfd303
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-12 18:24:53 +0530

    divide & conquer - assignment - solution

--- concurrency/demo-04.go
+++ concurrency/demo-04.go
@@ -1,11 +1,24 @@
 package main
 
-func sum(nos []int) {
-	/* sum the given numbers */
+import "fmt"
+
+func sum(nos []int, resultCh chan int) {
+	result := 0
+	for _, no := range nos {
+		result += no
+	}
+	resultCh <- result
 }
 
 func main() {
 	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
 	/* divide the data into two chunks and run 2 goroutine instances of the sum with the chunks and aggregate the results in the main func and print it */
+	firstSet := data[:len(data)/2]
+	secondSet := data[len(data)/2:]
+	resultCh := make(chan int)
+	go sum(firstSet, resultCh)
+	go sum(secondSet, resultCh)
+	total := <-resultCh + <-resultCh
+	fmt.Println("Total = ", total)
 
 }
