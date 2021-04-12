dbae5fe695bda194dfeddd0079be28a314f25904
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-12 18:13:48 +0530

    divide & conquer - assignment

---
+++ concurrency/demo-04.go
@@ -0,0 +1,11 @@
+package main
+
+func sum(nos []int) {
+	/* sum the given numbers */
+}
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+	/* divide the data into two chunks and run 2 goroutine instances of the sum with the chunks and aggregate the results in the main func and print it */
+
+}
